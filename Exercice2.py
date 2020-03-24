6a1cd114a13b2ca370e48d1d714a0dce861393be
Angel-Gab <angele.gaboriau@isen-ouest.yncrea.fr>
2020-03-24 19:24:36 +0100

    Exercice1

--- Exercice2.py
+++ Exercice2.py
@@ -9,36 +9,34 @@ class Complex :
         return(Complex(self.__Re + New.__Re, self.__Im + New.__Im))
 
     def __sub__(self,New):
-        return(Complex(self.__Re - New.__Re, self.__Im - New.__Im))
+        return(Complex(self.__Re - New.__Re , self.__Im - New.__Im))
 
     def __mul__(self,New):
-        return(Complex(self.__Re * New.__Re, self.__Im * New.__Im))
+        return Complex(self.__Re * New.__Re , self.__Im * New.__Im)
 
     def __truediv__(self,New):
-        return(Complex(self.__Re / New.__Re, self.__Im / New.__Im))
+        return(Complex(self.__Re / New.__Re , self.__Im / New.__Im))
 
-    def __abs__(self,New):
-        return(Complex(abs(self.__Re + New.__Re),abs( self.__Im + New.__Im)))
+    def __abs__(self):
+        return (self.__Re**2 + self.__Im**2)**1/2
 
     def __eq__(self,New):
-        return(Complex(self.__Re == New.__Re, self.__Im == New.__Im))
+        return(self.__Re == New.__Re and self.__Im == New.__Im)
 
     def __ne__(self,New):
-        return(Complex(self.__Re != New.__Re, self.__Im != New.__Im))
+        return self.__Re != New.__Re or self.__Im != New.__Im
 
 
 if __name__ == "__main__":
     c1 = Complex(13,11)
     c2 = Complex(12,13)
     c3 = c1 + c2
-    c4 = c1 != c2
-    c5 = c2 == c3
-    c6 = c2 / c3
-    c7 = c2*c3
+    print(c1 != c2)
+    print(c2 == c3)
+    c4 = c2 / c3
+    c5 = c2*c3
     print(c3)
     print(c4)
     print(c5)
-    print(c6)
-    print(c7)
 
 
